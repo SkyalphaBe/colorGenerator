e91249fc67dabd777fa14808d1a4b91945d34ef3
François Graux <frsgra28@gmail.com>
2023-01-23 12:42:05 +0100

    correction de la méthode de couleur + style + accueil

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,14 @@
 "use strict";
-const page = document.querySelector("body");
+const corp = document.querySelector("body");
+const generateBtn = document.getElementById("generateBtn");
+const menu = document.getElementsByClassName("menu")[0];
+let time = setInterval(() => {
+    let color = colorGenerator();
+    if (corp != undefined) {
+        corp.style.backgroundColor = color;
+        corp.style.transition = "all 3.5s";
+    }
+}, 1250);
 function randomNumber(maxvalue) {
     return Math.floor(Math.random() * maxvalue);
 }
@@ -7,13 +16,43 @@ function colorGenerator() {
     let firstNb = randomNumber(255);
     let secondeNb = randomNumber(255);
     let thirdNb = randomNumber(255);
-    let res = "#" + firstNb.toString() + secondeNb.toString() + thirdNb.toString();
+    let res = "#" + firstNb.toString(16) + secondeNb.toString(16) + thirdNb.toString(16);
     return res;
 }
-setInterval(() => {
-    let color = colorGenerator();
-    if (page != undefined) {
-        page.style.backgroundColor = color;
-        page.style.transition = "all 3s";
+generateBtn === null || generateBtn === void 0 ? void 0 : generateBtn.addEventListener("click", () => {
+    if (corp != undefined && generateBtn != undefined && menu != undefined) {
+        let color = colorGenerator();
+        generateBtn.style.display = "None";
+        let content = document.createElement("div");
+        let textField = document.createElement("input");
+        let divColor = document.createElement("div");
+        let changeColorBtn = document.createElement("button");
+        let saveBtn = document.createElement("button");
+        let divBtn = document.createElement("div");
+        content.className = "content";
+        textField.id = "hexaColor";
+        textField.setAttribute("type", "text");
+        textField.value = color;
+        divColor.id = "boxColor";
+        divColor.style.backgroundColor = color;
+        content.appendChild(divColor);
+        content.appendChild(textField);
+        changeColorBtn.textContent = "change color";
+        changeColorBtn.id = "changeBtn";
+        saveBtn.id = "saveBtn";
+        saveBtn.textContent = "save color";
+        divBtn.id = "divBtn";
+        divBtn.appendChild(changeColorBtn);
+        divBtn.appendChild(saveBtn);
+        menu.insertBefore(content, generateBtn);
+        menu.appendChild(divBtn);
+        corp.style.backgroundColor = color;
+        clearInterval(time);
+        changeColorBtn.addEventListener("click", () => {
+            let newColor = colorGenerator();
+            textField.value = newColor;
+            divColor.style.backgroundColor = newColor;
+            corp.style.backgroundColor = newColor;
+        });
     }
-}, 1250);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,14 @@
-const page = document.querySelector("body");
+const corp = document.querySelector("body");
+const generateBtn = document.getElementById("generateBtn")
+const menu = document.getElementsByClassName("menu")[0];
+
+let time = setInterval(()=>{
+    let color:string = colorGenerator();
+    if(corp != undefined){
+        corp.style.backgroundColor = color;
+        corp.style.transition = "all 3.5s"
+    }
+},1250);
 
 function randomNumber(maxvalue:number):number{
     return Math.floor(Math.random() * maxvalue);
@@ -9,15 +19,56 @@ function colorGenerator():string{
     let secondeNb:number = randomNumber(255);
     let thirdNb:number = randomNumber(255);
 
-    let res:string = "#"+firstNb.toString()+secondeNb.toString()+thirdNb.toString();
+    let res:string = "#"+firstNb.toString(16)+secondeNb.toString(16)+thirdNb.toString(16);
 
     return res;
 }
 
-setInterval(()=>{
-    let color:string = colorGenerator();
-    if(page != undefined){
-        page.style.backgroundColor = color;
-        page.style.transition = "all 3s"
+generateBtn?.addEventListener("click",()=>{
+    if(corp != undefined && generateBtn != undefined && menu != undefined){
+        let color:string = colorGenerator();
+        generateBtn.style.display = "None";
+
+        let content:HTMLElement = document.createElement("div");
+        let textField:HTMLInputElement = document.createElement("input");
+        let divColor:HTMLElement = document.createElement("div");
+        let changeColorBtn:HTMLElement = document.createElement("button");
+        let saveBtn:HTMLElement = document.createElement("button");
+        let divBtn: HTMLElement = document.createElement("div")
+
+        content.className = "content";
+
+        textField.id = "hexaColor";
+        textField.setAttribute("type","text");
+        textField.value=color;
+
+        divColor.id = "boxColor";
+        divColor.style.backgroundColor = color;
+
+        content.appendChild(divColor);
+        content.appendChild(textField);
+
+        changeColorBtn.textContent="change color";
+        changeColorBtn.id="changeBtn";
+        saveBtn.id="saveBtn";
+        saveBtn.textContent="save color";
+        divBtn.id="divBtn";
+        divBtn.appendChild(changeColorBtn);
+        divBtn.appendChild(saveBtn);
+
+        menu.insertBefore(content,generateBtn);
+        menu.appendChild(divBtn);
+        corp.style.backgroundColor = color;
+        clearInterval(time);
+
+        changeColorBtn.addEventListener("click",()=>{
+            let newColor:string=colorGenerator();
+            textField.value = newColor;
+            divColor.style.backgroundColor = newColor;
+            corp.style.backgroundColor = newColor;
+        });
+        
     }
-},1250);
+
+
+});
